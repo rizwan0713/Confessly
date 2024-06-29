fdf87660b60b079cb6cbcd45b7038502cf7210f4
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-06-30 02:48:23 +0530

    db connect

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,32 @@
+import mongoose, { Promise } from "mongoose";
+
+type ConnectionObject ={
+    isConnected?:number
+}
+
+const connection : ConnectionObject = {}
+
+async function dbConnect():Promise<void>{
+    if(connection.isConnected){
+        console.log("Already connectd to database");
+        return
+    }
+    try{
+        const db = await mongoose.connect(process.env.
+            MONGODb_URI || "",{})
+
+            connection.isConnected = db.connections[0].readyState
+            console.log("DB connected successfully");
+            console.log("db.connection",db.connections);
+            console.log("db console karte h",db);
+
+
+    }
+    catch(error){
+        console.log("Database Connection failed",error)
+       process.exit()
+    }
+    
+}
+
+export default dbConnect;
