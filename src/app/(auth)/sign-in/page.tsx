a26e585edf2d3178530e3079bc84ca34d5652b49
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-07-17 15:45:11 +0530

    updates

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-const page = () => {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>page</div>
+    <>
+      Not signed in <br />
+      <button className="bg-red-400 px-4 py-2 rounded m-3" onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default page
--- src/middleware.ts
+++ src/middleware.ts
@@ -16,10 +16,10 @@ export async function middleware(request: NextRequest) {
 
     )
     ) {
-           return NextResponse.redirect(new URL('dashboard',request.url))
+           return NextResponse.redirect(new URL('/dashboard',request.url))
         
     }
-  return NextResponse.redirect(new URL('/home', request.url))
+  // return NextResponse.redirect(new URL('/nome', request.url))
 }
  
 // See "Matching Paths" below to learn more
