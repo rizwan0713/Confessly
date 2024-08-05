ae3f49ddda9e18db0c0ebf429c8c7e9fc1365b5f
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-08-06 01:27:19 +0530

    Added Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { ThemeProvider } from '@/components/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
+  title: 'Confessly',
+  description: 'Real Confession from real people.',
 };
 
 interface RootLayoutProps {
