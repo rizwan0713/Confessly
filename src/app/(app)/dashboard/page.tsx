c6d591e2e0c5691d3a4eba450d7402c518fdee82
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-08-19 23:20:47 +0530

    overflow-x-hidden" in dash

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -156,7 +156,7 @@ function UserDashboard  () {
 
 
   return (
-    <div className=" dark:border-gray-500 dark:border-2  dark:bg-opacity-30	 my-8 mx-4 md:mx-8 lg:mx-auto p-6 dark:bg-gray-700  bg-white rounded w-full max-w-6xl">
+    <div className=" dark:border-gray-500 dark:border-2  dark:bg-opacity-30	 my-8 mx-4 md:mx-8 lg:mx-auto p-6 dark:bg-gray-700  bg-white rounded w-full max-w-6xl  overflow-x-hidden">
     <h1 className="text-4xl font-bold mb-4 dark:text-white">
       User Dashboard
     </h1>
