3a9de831196180e245a359193475c84e19f179eb
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-08-19 22:09:14 +0530

    update length of message to 100 in schema as well as suggest message route

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const messageSchema = z.object({
     content:z.string()
     .min(10,{message:"Content must be at least of 10 characters"})
-    .max(80,{message:"Content must be no longer than 50 characters"})
+    .max(100,{message:"Content must be no longer than 100 characters"})
 })
