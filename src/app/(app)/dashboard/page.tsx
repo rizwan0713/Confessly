210a34e434cb7f9cc60cce21f112a554a7eae49d
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-08-19 23:13:14 +0530

    update get message error ji

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -81,7 +81,7 @@ function UserDashboard  () {
             toast({
                 title:"Error ji",
                 description:axiosError.response?.data.message || "failed to get Messages setting",
-                variant:"destructive"
+                variant:"default"
             })
         } finally{
             setIsLoading(false)
