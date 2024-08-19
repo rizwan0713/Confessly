8fd1c37dc9fac8140b7f9f51717843c95a423b2e
rizwan0713 <rizwansalmanirsa@gmail.com>
2024-08-19 21:53:26 +0530

    remove  '/' in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,10 +31,10 @@ export async function middleware(request: NextRequest) {
       
                       url.pathname.startsWith('/sign-in') ||
                       url.pathname.startsWith('/sign-up') ||
-                      url.pathname.startsWith('/verify/:path*') ||
+                      url.pathname.startsWith('/verify/:path*') 
                     
                       // url.pathname.startsWith('/')
-                      url.pathname === '/'
+                      // url.pathname === '/'
                    )
     ) {  
                                                                             // console.log("request.url is:",request.url)
